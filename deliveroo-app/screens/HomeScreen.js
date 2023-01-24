1fa67789403b85da57816ed93ce57ded1a1b4fb3
Tea <controlalttea@gmail.com>
2023-01-23 20:26:05 -0800

    33:58 SafeAreaView does not work with Galaxy S9 but might work on newer phones

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-export default function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+const HomeScreen = () => { 
+    const navigation = useNavigation();
+    
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <Text className="text-red-500">HomeScreen</Text>
+            {/* HEADER */}
+            <View>
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru'
+                    
+                    }}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                    />
+            </View>
+        </SafeAreaView>
+        );
 }
+
+export default HomeScreen
