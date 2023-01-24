c002ddd1fcea897570c5b9bb01d649301adf5be8
Tea <controlalttea@gmail.com>
2023-01-23 20:00:42 -0800

    setting up screens for react native

--- deliveroo-app/App.js
+++ deliveroo-app/App.js
@@ -1,20 +1,17 @@
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500">Open up App.js to start working on your Android</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* SCREENS */}
+        <Stack.Screen name='Home' component={HomeScreen}/>
+      </Stack.Navigator>
+   </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
