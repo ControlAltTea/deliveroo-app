ce3bd5b5dc482dc096e6b6fcae4a9212688103a0
Tea <controlalttea@gmail.com>
2023-01-24 17:00:48 -0800

    tailwindw not being picked up...

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,7 +1,6 @@
-import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
+import { View, Text, Image, StyleSheet, Platform, StatusBar, SafeAreaView} from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { StatusBar } from 'expo-status-bar';
 
 const HomeScreen = () => { 
     const navigation = useNavigation();
@@ -13,29 +12,29 @@ const HomeScreen = () => {
     }, []);
 
     return (
-        <SafeAreaView>
-            <Text className="text-red-500">HomeScreen</Text>
+        <SafeAreaView style={styles.container}>
+            <Text className="text-red-500">
             {/* HEADER */}
-            <View>
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
                 <Image
                     source={{
                         uri: 'https://links.papareact.com/wru',
                     }}
-                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                    className={`bg-gray-300 p-4 rounded-full`}
                     />
             </View>
             <View>
-                <Text>Deliver Now!</Text>
-                <Text>Current Location</Text>
-            </View>
+                <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                <Text className="font-bold text-xl">Current Location</Text>
+                </View>
+            </Text>
         </SafeAreaView>
         );
 }
 
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight
+        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
     }
 })
 
