722a6349f2025f176c64c978ac2019069a2790bf
Tea <controlalttea@gmail.com>
2023-01-24 16:20:20 -0800

    emulator in Android Studio now works

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar';
 
 const HomeScreen = () => { 
     const navigation = useNavigation();
@@ -18,14 +19,24 @@ const HomeScreen = () => {
             <View>
                 <Image
                     source={{
-                        uri: 'https://links.papareact.com/wru'
-                    
+                        uri: 'https://links.papareact.com/wru',
                     }}
                     className="h-7 w-7 bg-gray-300 p-4 rounded-full"
                     />
             </View>
+            <View>
+                <Text>Deliver Now!</Text>
+                <Text>Current Location</Text>
+            </View>
         </SafeAreaView>
         );
 }
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight
+    }
+})
 
 export default HomeScreen
