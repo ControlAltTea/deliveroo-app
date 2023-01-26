bc9d853994100f5c469fab5e7c290d0149aeebf3
Tea <controlalttea@gmail.com>
2023-01-26 15:27:16 -0800

    flex-1 is not working for some reason...

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, Platform, StatusBar, SafeAreaView } from 'react-native'
+import { View, Text, Image, StyleSheet, Platform, StatusBar, SafeAreaView, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
@@ -26,15 +26,26 @@ const HomeScreen = () => {
                         source={{
                             uri: 'https://links.papareact.com/wru',
                         }}
-                        className={`bg-gray-300 p-4 rounded-full`}
+                        className={`h-7 w-7 bg-gray-300 p-4 rounded-full`}
                     />
-                    <View>
+                    <View className="flex-1">
                         <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
                         <Text className="font-bold text-xl">Current 
                         Location
                             <ChevronDownIcon size={20} color="#00CCB8"/>
                         </Text>
                     </View>
+                    <UserIcon size={35} color="#00CCB8"/>
+                </View>
+
+                {/* SEARCH */}
+                <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                    <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                        {/* <SearchIcon color="gray" size={20} />
+                        <TextInput placeholder='Restaurants and Cuisines'
+                        keyboardType='default'/> */}
+                    </View>
+                    {/* <AdjustmentsIcon /> */}
                 </View>
             </Text>
         </SafeAreaView>
@@ -43,7 +54,8 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
+        backgroundColor: "white",
+        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
     }
 })
 
