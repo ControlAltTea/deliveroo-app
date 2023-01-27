f1dd2faddc87193c4de0deed5763ee2361efb368
Tea <controlalttea@gmail.com>
2023-01-26 18:13:01 -0800

    not sure why the flex isn't working

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -8,12 +8,8 @@ import {
     AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline';
 
-let screenWidth;
-
 const HomeScreen = () => {
     const navigation = useNavigation();
-    const { height, width, scale, fontScale } = useWindowDimensions();
-    screenWidth = width;
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -25,31 +21,35 @@ const HomeScreen = () => {
         <SafeAreaView style={styles.container}>
             <Text>
                 {/* HEADER */}
-                <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
                     <Image
                         source={{
                             uri: 'https://links.papareact.com/wru',
                         }}
                         className="h-7 w-7 bg-gray-300 p-4 rounded-full"
                     />
-                    <View className={styles.view}>
-                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                        <Text className="font-bold text-xl">Current 
-                        Location
-                            <ChevronDownIcon size={20} color="#00CCB8"/>
+                    <View className="android:flex-1">
+                        <Text className="font-bold text-gray-400 text-xs">
+                            Deliver Now!
+                        </Text>
+                        <Text className="font-bold text-xl">
+                            Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB"/>
                         </Text>
                     </View>
-                    <UserIcon size={35} color="#00CCB8"/>
+                    <UserIcon size={35} color="#00CCBB"/>
                 </View>
 
                 {/* SEARCH */}
-                <View className="flex-row items-center space-x-2 pb-2 mx-4">
-                    <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+                    <View className="w-[300px] flex-row space-x-2 bg-gray-200 p-3">
                         <MagnifyingGlassIcon color="gray" size={20} />
-                        <TextInput placeholder='Restaurants and Cuisines'
-                        keyboardType='default'/>
+                        <TextInput
+                            placeholder='Restaurants and Cuisines'
+                            keyboardType='default'
+                            color="red"/>
                     </View>
-                    <AdjustmentsVerticalIcon />
+                    <AdjustmentsVerticalIcon color="#00CCB8" size={20} />
                 </View>
             </Text>
         </SafeAreaView>
@@ -60,9 +60,6 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "white",
         paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
-    },
-    view: {
-        flex: 1,
     }
 })
 
