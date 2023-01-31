08aac7bcee5350a06c62627d540663d73ae2c62f
Tea <controlalttea@gmail.com>
2023-01-30 19:46:07 -0800

    not sure why I'm getting errors about babel but it's running both on Android and the sanity server is running fine

--- deliveroo-app/sanity/schemas/restaurant.js
+++ deliveroo-app/sanity/schemas/restaurant.js
@@ -8,7 +8,35 @@ export default {
             name: "name",
             type: "string",
             title: "Restaurant name",
-            valigation: (Rule) => Rule.required(),
+            validation: (Rule) => Rule.required(),
         },
+        {
+            name: "short_description",
+            type: "string",
+            title: "short description",
+            validation: (Rule) => Rule.max(200),
+        },
+        {
+            name: "image",
+            type: "image",
+            title: "image of the restaurant",
+        },
+        {
+            name: "lat",
+            type: "number",
+            title: "Latitude of the restaurant",
+        },
+        {
+            name: "long",
+            type: "number",
+            title: "longitude of the restaurant",
+        },
+        {
+            name: "address",
+            type: "string",
+            title: "restaurant address",
+            validation: (Rule) => Rule.required()
+        }
+
     ],
 }
--- deliveroo-app/sanity/schemas/schema.js
+++ deliveroo-app/sanity/schemas/schema.js
@@ -1,5 +1,6 @@
-import { createSchema } from "sanity";
+import createSchema from "'all:part:@sanity/base/schema-creator'";
 import schemaTypes from 'all:part:@sanity/base/schema-type';
+
 import blockContent from "./blockContent";
 import category from "./category";
 import post from "./post";
