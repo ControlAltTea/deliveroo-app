e9bbff1480c73b96953e9bc859ff6ff16540b1ce
Tea <controlalttea@gmail.com>
2023-02-01 14:51:32 -0800

    changed the format of the sanity.cli.js to accomodate the useEffect fetch request throwing an error about now having a fetch function available from the @sanity/client library. current the query in HomeScreen logs the restaurants in the sanity database

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,92 +1,124 @@
-import { View, Text, Image, StyleSheet, Platform, StatusBar, TextInput, ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
 import {
-    UserIcon,
-    ChevronDownIcon,
-    MagnifyingGlassIcon,
-    AdjustmentsVerticalIcon,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline';
 import Categories from '../components/Categories';
 import FeaturedRow from '../components/FeaturedRow';
+import client from '../sanity/sanity.cli';
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        })
-    }, []);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-                {/* HEADER */}
-                <View className="flex flex-row pb-3 items-center mx-4 space-x-2">
-                    <Image
-                        source={{
-                            uri: 'https://links.papareact.com/wru',
-                        }}
-                        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-                    />
-                    <View className={` flex-1`}>
-                        <Text className="font-bold text-gray-400 text-xs">
-                            Deliver Now!
-                        </Text>
-                        <Text className="font-bold text-xl">
-                            Current Location
-                        <ChevronDownIcon size={20} color="#00CCBB"/>
-                        </Text>
-                    </View>
-                    <UserIcon className="items-center" size={35} color="#00CCBB"/>
-                </View>
+    const query = `*[_type=="featured"] {
+            restaurants[]->{
+                ...,
+                dishes[]->,
+                type->{
+                name
+            }
+        },    
+    }`;    
 
-                {/* SEARCH */}
-                <View className="flex-row items-center space-x-2 pb-2 mx-4">
-                    <View className="flex-1 flex-row space-x-2 bg-gray-200 p-3">
-                        <MagnifyingGlassIcon color="gray" size={20} />
-                        <TextInput
-                            placeholder='Restaurants and Cuisines'
-                            keyboardType='default'
-                            color="red"/>
-                    </View>
-                    <AdjustmentsVerticalIcon color="#00CCB8" size={20} />
-                </View>
+  useEffect(() => {
+      client
+        .fetch(query)
+        .then((data) => {
+             setFeaturedCategories(data);
+        });
+  }, []);
 
-                {/* BODY */}
-            <ScrollView className="bg-gray-100"
-                contentContainerStyle={{
-                paddingBottom: 100,
-            }}>
-                {/* COMPONENT -CATEGORIES */}
-                <Categories />
-                {/* FEATURED ROWS */}
-                <FeaturedRow
-                    id="123"
-                    title="Featured"
-                    description="Paid placements from our partners"
-                />
-                <FeaturedRow
-                    id="1234"
-                    title="Tasty Discounts"
-                    description="Paid placements from our partners"
-                />
-                <FeaturedRow
-                    id="12345"
-                    title="Offers near you"
-                    description="Paid placements from our partners"
-                />
-                </ScrollView>
-        </SafeAreaView>
-    );
-}
+    console.log(featuredCategories);
+    
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* HEADER */}
+      <View className='flex flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+        />
+        <View className={` flex-1`}>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
+        </View>
+        <UserIcon className='items-center' size={35} color='#00CCBB' />
+      </View>
+
+      {/* SEARCH */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-1 flex-row space-x-2 bg-gray-200 p-3'>
+          <MagnifyingGlassIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restaurants and Cuisines'
+            keyboardType='default'
+            color='red'
+          />
+        </View>
+        <AdjustmentsVerticalIcon color='#00CCB8' size={20} />
+      </View>
+
+      {/* BODY */}
+      <ScrollView
+        className='bg-gray-100'
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* COMPONENT -CATEGORIES */}
+        <Categories />
+        {/* FEATURED ROWS */}
+        <FeaturedRow
+          id='123'
+          title='Featured'
+          description='Paid placements from our partners'
+        />
+        <FeaturedRow
+          id='1234'
+          title='Tasty Discounts'
+          description='Paid placements from our partners'
+        />
+        <FeaturedRow
+          id='12345'
+          title='Offers near you'
+          description='Paid placements from our partners'
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
-    }
-})
+  container: {
+    backgroundColor: 'white',
+    paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
