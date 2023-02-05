ea5b8102be09a47a167e75440525e2a26c1a584b
Tea <controlalttea@gmail.com>
2023-02-04 16:08:54 -0800

    Added short description, icons, etc.

--- deliveroo-app/screens/RestaurantScreen.js
+++ deliveroo-app/screens/RestaurantScreen.js
@@ -3,8 +3,18 @@ import React, { useLayoutEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text, StyleSheet, StatusBar, ScrollView, Image, TouchableOpacity } from 'react-native'
 import {
-    ArrowLeftIcon
+    ArrowLeftIcon,
+    StarIcon,
+    MapPinIcon
 } from 'react-native-heroicons/solid';
+import {
+    UserIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+    MagnifyingGlassIcon,
+    AdjustmentsVerticalIcon,
+    QuestionMarkCircleIcon,
+} from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity/sanity.cli';
 
 
@@ -47,6 +57,38 @@ const RestaurantScreen = () => {
                         size={20}
                         color="#00CCBB"
                     />
+                </TouchableOpacity>
+            </View>
+            <View className="bg-white">
+                <View className="px-4 pt-4">
+                    <Text className="text-3xl font-bold">{title}</Text>
+                    <View className="flex-row space-x-2 my-1">
+                        <View className="flex-row items-center space-x-1">
+                            <StarIcon color="green" opacity={0.5} size={22} />
+                            <Text className="text-xs text-gray-500">
+                                <Text className="text-green-500">{rating} </Text>. {genre}
+                            </Text>
+                        </View>
+                        <View className="flex-row items-center space-x-1">
+                            <MapPinIcon color="gray" opacity={0.5} size={22} />
+                            <Text className="text-green-500">Nearby  </Text>. {address}
+                        </View>
+                    </View>
+                    <Text
+                        className="text-gray-500 mt-2 pb-4"
+                    >
+                        {short_description}
+                    </Text>
+                </View>
+                <TouchableOpacity>
+                    <QuestionMarkCircleIcon
+                            color="gray"
+                            opacity={0.6}
+                            size={20}    
+                        />
+                        <ChevronRightIcon
+                            color="#00CCBB"
+                        />
                 </TouchableOpacity>
             </View>
         </ScrollView>
