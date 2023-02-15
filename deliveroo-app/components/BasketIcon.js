d81cc0e435e2769c8aae0c5b3053c307cc0bef74
Tea <controlalttea@gmail.com>
2023-02-14 17:43:11 -0800

    styling the basket screen

--- deliveroo-app/components/BasketIcon.js
+++ deliveroo-app/components/BasketIcon.js
@@ -9,6 +9,9 @@ export const BasketIcon = () => {
     const items = useSelector(selectBasketItems);
     const navigation = useNavigation()
     const basketTotal = useSelector(selectBasketTotal);
+
+    if (items.length === 0) return null;
+
     return (
         <View className="absolute bottom-10 w-full z-50">
             <TouchableOpacity onPress={() =>navigation.navigate("Basket")} className="bg-[#00CCB8] p-4 rounded-lg flex-row items-center space-x-1">
--- deliveroo-app/screens/BasketScreen.js
+++ deliveroo-app/screens/BasketScreen.js
@@ -1,24 +1,48 @@
 import { useNavigation } from '@react-navigation/native'
-import React, {useMemo} from 'react'
-import { View, Text } from 'react-native'
+import React, {useMemo, useState, useEffect} from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectBasketItems } from '../features/basketSlice';
 import { selectRestaurant } from '../features/restaurantSlice';
+import {XCircleIcon} from "react-native-heroicons/solid"
 
 const BasketScreen = () => {
     const navigation = useNavigation();
     const restaurant = useSelector(selectRestaurant);
     const items = useSelector(selectBasketItems);
+    const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
     const dispatch = useDispatch();
-    
+
     useMemo(() => {
-        
+        const groupedItems = items.reduce((results, item) => {
+            (results[item.id] = results[item.id] || []).push(item);
+            return results
+        }, {});
+
+        setGroupedItemsInBasket(groupedItems);
     }, [items]);
 
     return (
-        <View>
-            <Text>BasketScreen</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <View>
+                    <View>
+                        <Text className="text-lg font-bold text-center">
+                            Basket
+                        </Text>
+                        <Text className="text-center text-gray-400">
+                            {restaurant.title}
+                        </Text>
+                    </View>
+                    <TouchableOpacity
+                        onPress={navigation.goBack}
+                        className="rounded-full bg-gray-100 absolute top-3 right-5"
+                    >
+                        <XCircleIcon color="#00CCB8" height={50} width={50} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
