9f745f5eed560ae9c7e8195a1d7dd4d13eccf938
Tea <controlalttea@gmail.com>
2023-02-17 11:40:50 -0800

    added preparing screen gif

--- deliveroo-app/screens/PreparingOrderScreen.js
+++ deliveroo-app/screens/PreparingOrderScreen.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native'
+import * as Animatable from "react-native-animatable"
 
 const PreparingOrderScreen = () => {
   return (
       <SafeAreaView style={styles.container} className="bg-[#00CCB8] flex-1 justify-center items-center">
-          <Text>
-              PreparingOrderScreen 
-          </Text>
+          <Animatable.Image
+              source={require("../assets/deliveryGif.gif")}
+              animation="slideInUp"
+              iterationCount={1}
+              className="h-96 w-96"
+          />
+          
     </SafeAreaView>
     )
 }
