2a65d4c2bbea160a6b4f6b41034ab953f150083a
Tea <controlalttea@gmail.com>
2023-02-17 13:17:28 -0800

    added new views to dilvery screen

--- deliveroo-app/screens/DeliveryScreen.js
+++ deliveroo-app/screens/DeliveryScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
@@ -10,16 +10,29 @@ const DeliveryScreen = () => {
     const restaurant = useSelector(selectRestaurant);
     return (
         <View className="bg-[#00CCB8] flex-1">
-            <SafeAreaView>
+            <SafeAreaView style={styles.container} className="flex-row justify-between items-center p-5">
                 <View>
                     <TouchableOpacity onPress={() => navigation.navigate("Home")}>
                         <XMarkIcon color="white" size={30} />
                     </TouchableOpacity>
-                    <Text>Order Help</Text>
+                    <Text className="font-light text-white text-lg">Order Help</Text>
+                </View>
+                
+                <View>
+                    <View>
+                        <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+                        <Text className="text-2xl font-bold">45-55 Minutes</Text>
+                    </View>
                 </View>
             </SafeAreaView>
         </View>
         )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
+    },
+});
 
 export default DeliveryScreen
