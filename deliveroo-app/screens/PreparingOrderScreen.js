dc5b4332e568a813a555c98592cdeedef174d38b
Tea <controlalttea@gmail.com>
2023-02-17 12:10:05 -0800

    added Animatable and Progress library

--- deliveroo-app/screens/PreparingOrderScreen.js
+++ deliveroo-app/screens/PreparingOrderScreen.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native'
 import * as Animatable from "react-native-animatable"
+import * as Progress from "react-native-progress"
 
 const PreparingOrderScreen = () => {
-  return (
-      <SafeAreaView style={styles.container} className="bg-[#00CCB8] flex-1 justify-center items-center">
-          <Animatable.Image
-              source={require("../assets/deliveryGif.gif")}
-              animation="slideInUp"
-              iterationCount={1}
-              className="h-96 w-96"
-          />
-          
-    </SafeAreaView>
+    return (
+        <SafeAreaView style={styles.container} className="bg-[#00CCB8] flex-1 justify-center items-center">
+            <Animatable.Image
+                source={require("../assets/deliveryGif.gif")}
+                animation="slideInUp"
+                iterationCount={1}
+                className="h-96 w-96"
+            />
+            <Animatable.Text
+                animation="slideInUp"
+                iterationCount={1}
+                className="text-lg my-10 text-white font-bold text-center"
+            >
+                Waiting for Restaurant to accept your order!
+            </Animatable.Text>
+
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
         paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 5,
     },
 });
